8059359049a12e69b200e80a78f205732a352e6a
Albin Stjerna <albin@eval.nu>
2012-07-21 12:45:17 +0200

    Initial commit.

---
+++ src/com/albinstjerna/ananke/ChooserActivity.java
@@ -0,0 +1,18 @@
+package com.albinstjerna.ananke;
+
+//import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+
+public class ChooserActivity extends FragmentActivity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chooser);
+    }
+}
