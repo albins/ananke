461f0cc4ebf7f7cdcb44bb03d7f147537e9d7975
Albin Stjerna <albin@eval.nu>
2012-07-21 13:53:14 +0200

    Implement simple binary desicionmaking.

--- src/com/albinstjerna/ananke/BinaryChoiceFragment.java
+++ src/com/albinstjerna/ananke/BinaryChoiceFragment.java
@@ -7,10 +7,12 @@ import android.view.ViewGroup;
 import android.view.View;
 
 public class BinaryChoiceFragment extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, 
         Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.binarychoice, container, false);
     }
+
 }
--- src/com/albinstjerna/ananke/ChooserActivity.java
+++ src/com/albinstjerna/ananke/ChooserActivity.java
@@ -5,14 +5,30 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.view.View;
+import java.util.Random;
+import android.content.Intent;
 
 public class ChooserActivity extends FragmentActivity
 {
+    public final static String EXTRA_MESSAGE = "com.albinstjerna.ananke.MESSAGE";
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chooser);
+    }
+
+    /** Send the decision to the displayer view. */
+    public void sendMessage(View view) {
+        Random randgen = new Random();
+        int random_number = randgen.nextInt(2);
+        Intent intent = new Intent(this, DisplayerActivity.class);
+
+        // The funky concatenation below is to force the number to a string.
+        intent.putExtra(EXTRA_MESSAGE, "" + random_number);
+        startActivity(intent);
     }
 }
