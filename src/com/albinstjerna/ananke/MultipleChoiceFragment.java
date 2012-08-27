cb5b1126a692296504535e0085d4ac3fbd8d3238
Albin Stjerna <albin@eval.nu>
2012-08-27 11:32:20 +0200

    More clean-up (old commented-out code).

--- src/com/albinstjerna/ananke/MultipleChoiceFragment.java
+++ src/com/albinstjerna/ananke/MultipleChoiceFragment.java
@@ -1,7 +1,6 @@
 package com.albinstjerna.ananke;
 
 import android.os.Bundle;
-//import android.support.v4.app.Fragment;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
