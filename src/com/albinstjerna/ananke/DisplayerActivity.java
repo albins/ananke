2de55c1795a411c57e396b6db26aa7dded08867f
Albin Stjerna <albin@eval.nu>
2012-08-27 11:31:42 +0200

    More clean-up.

--- src/com/albinstjerna/ananke/DisplayerActivity.java
+++ src/com/albinstjerna/ananke/DisplayerActivity.java
@@ -2,9 +2,6 @@ package com.albinstjerna.ananke;
 
 import android.app.Activity;
 import android.os.Bundle;
-// import android.support.v4.app.Fragment;
-// import android.support.v4.app.FragmentActivity;
-// import android.support.v4.app.FragmentManager;
 import android.content.Intent;
 import android.widget.TextView;
 import android.view.Gravity;
